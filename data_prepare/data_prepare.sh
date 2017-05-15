2f9f39aba349ad53680e5f036040b23b2b605755
gsprochette <gaspar.rochette@gmail.com>
2017-05-15 12:14:02 -0400

    update data_prepare.sh -- error in name of python file called

--- data_prepare/data_prepare.sh
+++ data_prepare/data_prepare.sh
@@ -4,5 +4,5 @@ module purge
 module load python
 module load deeplearning
 
-time python data_prepare.py --rawdata $HOME'/rawdata' --data $HOME'/lhc_gnn/data' --stdout advance.out
+time python preparedata.py --rawdata $HOME'/rawdata' --data $HOME'/lhc_gnn/data' --stdout advance.out
 echo 'DONE\n'
