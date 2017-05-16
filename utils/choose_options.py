45bcf9ff4ba1f38d746b63d5f6e5f00fba08e0d8
gsprochette <gaspar.rochette@gmail.com>
2017-05-16 12:09:25 -0400

    update choose_options -- fixed name of MarginRanking option

--- utils/choose_options.py
+++ utils/choose_options.py
@@ -6,7 +6,7 @@ from Atlas.optim.optimizer import SGDOptimizer, AdamOptimizer
 def loss(loss):
     if loss == 'BCE':
         criterion = binary_cross_entropy
-    elif loss == 'HingeEmbedding':
+    elif loss == 'MarginRanking':
         criterion = MarginRankingLoss()
     return criterion
 
