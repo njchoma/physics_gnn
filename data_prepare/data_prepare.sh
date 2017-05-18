23a25edd761c04b9a19e2000db42061f3b789deb
gsprochette <gaspar.rochette@gmail.com>
2017-05-18 17:53:30 -0400

    update data_prepare.sh to previous modifications

--- data_prepare/data_prepare.sh
+++ data_prepare/data_prepare.sh
@@ -4,5 +4,5 @@ module purge
 module load python
 module load deeplearning
 
-time python preparedata.py --rawdata $HOME'/rawdata' --data $HOME'/lhc_gnn/data' --stdout advance.out --dotrain --dotest
+time python preparedata.py --rawdata $HOME'/rawdata' --data $HOME'/lhc_gnn/data' --stdout advance.out --dotrain --dotest --wftrain --wftest --l2nntrain --l2nntest
 echo 'DONE\n'
