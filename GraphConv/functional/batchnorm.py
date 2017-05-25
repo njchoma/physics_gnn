2889e4bce5fb34026278dadee8f45b988a382e97
gsprochette <gaspar.rochette@gmail.com>
2017-05-25 13:29:43 -0400

    updated spatial batchnorm -- better handling of cases where std=0

--- GraphConv/functional/batchnorm.py
+++ GraphConv/functional/batchnorm.py
@@ -1,4 +1,23 @@
 import torch
+from torch.autograd import Variable
+
+
+def batchnorm_old(x, axis=0):
+    """renormalize independantly each column of input.
+    input : x variable of size batch * 1 * n * d_out
+    output : y = (x - E(x)) / sqrt(var(x)) of same size
+    """
+
+    # Compute empirical expectancy and substract
+    ex = x.mean(axis)  # axis representing the event length
+    x = x - ex.expand_as(x)
+
+    # Compute empirical standard deviation and divide
+    std = torch.sqrt((x * x).mean(axis))
+    std = std + std.mean().expand_as(std) / 10000  # avoids division by zero
+    x = x / std.expand_as(x)
+
+    return x
 
 
 def batchnorm(x, axis=0):
@@ -13,7 +32,10 @@ def batchnorm(x, axis=0):
 
     # Compute empirical standard deviation and divide
     std = torch.sqrt((x * x).mean(axis))
-    std = std + std.mean().expand_as(std) / 10000  # avoids division by zero
+    zeros = (std == 0)
+    if isinstance(zeros, Variable):
+        zeros = zeros.detach()
+    std = std + zeros.type_as(std)  # avoids division by zero
     x = x / std.expand_as(x)
 
     return x
