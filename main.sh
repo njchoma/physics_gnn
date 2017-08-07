ac804feef6f2c7add36b9f4a940f22d51c424302
gsprochette <gaspar.rochette@gmail.com>
2017-08-07 17:23:34 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,11 +4,13 @@
 # Uncomment a line to use it.
 
 # Network using a single kernel as described in "QCD-Aware Recursive Neural Networks for Jet Physics"
-# python script/main.py --name test_QCDAware --kernel QCDAware --data NYU --fm 24 --depth 8 --nbtrain 100000 --nbtest 5000 --nbprint 5000 --lr 0.03 --lrdecay 0.9
+python script/main.py --name test_QCDAwareMeanNorm --kernel QCDAwareMeanNorm --data NYU --fm 64 --depth 12 --nbtrain 1000000 --nbtest 1000000 --nbprint 20000 --lr 0.015 --lrdecay 0.9
+
+# Other Networks
+# pass
+
+# ########## Still being tested : ########## #
 
 # Network using multiple kernels as described in "QCD-Aware Recursive Neural Networks for Jet Physics". Those kernels are the same for all layers.
-python script/main.py --name test_LayerQCDAware --kernel LayerQCDAware --data NYU --fm 24 --edge_fm 4 --depth 8 --nbtrain 100000 --nbtest 5000 --nbprint 5000 --lr 0.03 --lrdecay 0.88
+# python script/main.py --name test_LayerQCDAware --kernel LayerQCDAware --data NYU --fm 24 --edge_fm 4 --depth 8 --nbtrain 100000 --nbtest 5000 --nbprint 5000 --lr 0.03 --lrdecay 0.88
 
-
-# Other Network
-# pass
