7911c9298cc77b6d5a009c03c9ee8790db4766ad
Nicholas Choma <nc2201@cs.nyu.edu>
2018-03-21 16:18:14 -0400

    Update model save: only save if test 1/FPR increases

--- script/experiment_handler.py
+++ script/experiment_handler.py
@@ -11,70 +11,86 @@ import loading.model.model_parameters as param
 
 
 def train_model(train_X, train_y, train_w, test_X, test_y, test_w):
-    """Loads data, recover network then train, test and save network"""
+  """Loads data, recover network then train, test and save network"""
 
 
-    net = build_model.make_net_if_not_there(param.args, param.args.first_fm, param.args.savedir)
-    logging.info(net)
+  net = build_model.make_net_if_not_there(param.args, param.args.first_fm, param.args.savedir)
+  logging.info(net)
 
-    if param.args.cuda:
-        net = net.cuda()
-        logging.warning('Working on GPU')
-    else:
-        net = net.cpu()
-        logging.warning('Working on CPU')
+  if param.args.cuda:
+    net = net.cuda()
+    logging.warning('Working on GPU')
+  else:
+    net = net.cpu()
+    logging.warning('Working on CPU')
 
-    criterion = nn.functional.binary_cross_entropy
+  criterion = nn.functional.binary_cross_entropy
 
-    for epoch in range(50):
-        t0 = time.time()
-        logging.info('learning rate : {}'.format(param.args.lrate))
-        optimizer = torch.optim.Adamax(net.parameters(), lr=param.args.lrate)
+  # Track best model performance
+  param.args.bestInvFpr = 0.0
+  param.args.bestAuc = 0.0
 
-        epoch_loss_avg = model.train_net(net, train_X, train_y, train_w, criterion, optimizer)
-        param.args.lrate *= param.args.lrdecay
-        logging.info(
-            param.args.name + ' loss epoch {} : {}'.format(epoch + 1, epoch_loss_avg))
+  for epoch in range(50):
+    t0 = time.time()
+    logging.info('\nLearning rate: {0:.3g}'.format(param.args.lrate))
+    optimizer = torch.optim.Adamax(net.parameters(), lr=param.args.lrate)
 
-        # Model performance on subset of training data
-        score_train, loss_train, fpr50_train = model.test_net(net, train_X[:param.args.nbtest], train_y[:param.args.nbtest], train_w[:param.args.nbtest], criterion, "train")
-        logging.info(
-            param.args.name + ' epoch {}. train : '.format(epoch + 1) +
-            'AUC {: >.3E}'.format(score_train)
-            + ' -- loss {: >.3E}'.format(loss_train)
-            + ' -- FPR {: >.3E}'.format(fpr50_train)
-        )
+    epoch_loss_avg = model.train_net(net, train_X, train_y, train_w, criterion, optimizer)
+    param.args.lrate *= param.args.lrdecay
+    logging.info(param.args.name+' loss epoch {} : {}'.format(epoch+1,epoch_loss_avg))
 
-        # Model performance on test data
-        score_test, loss_test, fpr50_test = model.test_net(net, test_X, test_y, test_w, criterion, "test")
-        logging.info(
-            param.args.name + ' epoch {}.  test : '.format(epoch + 1) +
-            'AUC {: >.3E}'.format(score_test)
-            + ' -- loss {: >.3E}'.format(loss_test)
-            + ' -- FPR {: >.3E}'.format(fpr50_test)
-        )
+    # Model performance on subset of training data
+    auc_train, loss_train, fpr_train = model.test_net(net, train_X[:param.args.nbtest], train_y[:param.args.nbtest], train_w[:param.args.nbtest], criterion, "train")
 
-        with open(path.join(param.args.savedir, param.args.name + '.csv'), 'a') as fileres:
-            fileres.write(
-                str(param.args.lrate) + ','
-                + str(loss_train) + ','
-                + str(loss_test) + ','
-                + str(score_train) + ','
-                + str(score_test) + ','
-                + str(1 / fpr50_train)  + ','
-                + str(1 / fpr50_test)  + ','
-                + str(epoch_loss_avg) + '\n'
-            )
+    # Model performance on test data
+    auc_test, loss_test, fpr_test = model.test_net(net, test_X, test_y, test_w, criterion, "test")
 
-        logging.info("Epoch took {} seconds".format(int(time.time()-t0)))
-        try:
-          param.save_args()
-          with open(path.join(param.args.savedir, param.args.name + '.pkl'), 'wb') as fileout:
-            # net -> cpu in case later running without cuda
-            pickle.dump(net.cpu(), fileout)
-            if (param.args.cuda):
-              net.cuda()
-          logging.warning('Model saved\n')
-        except:
-          logging.error("Issue saving model or model parameters")
-          exit()
+    # Log eval performance
+    print_epoch_info(epoch, "train", loss_train, auc_train, (1/fpr_train))
+    print_epoch_info(epoch, " test", loss_test,  auc_test,  (1/fpr_test))
+
+    # Log epoch information
+    with open(path.join(param.args.savedir, param.args.name + '.csv'), 'a') as fileres:
+      fileres.write(
+                    str(param.args.lrate) + ','
+                    + str(loss_train) + ','
+                    + str(loss_test) + ','
+                    + str(auc_train) + ','
+                    + str(auc_test) + ','
+                    + str(1 / fpr_train)  + ','
+                    + str(1 / fpr_test)  + ','
+                    + str(epoch_loss_avg) + '\n'
+                    )
+
+    # Save model if beat previous best
+    matchOrBeatFpr = ((1/(fpr_test+10**-20)) > param.args.bestInvFpr)
+    beatAuc = (auc_test > param.args.bestAuc)
+    if matchOrBeatFpr or (matchOrBeatFpr and beatAuc):
+      logging.info("\nBeat previous best. Saving...")
+      param.args.bestInvFpr = (1/(fpr_test+10**-20))
+      param.args.bestAuc = auc_test
+      save_model(net)
+
+      logging.info("Epoch took {} seconds\n".format(int(time.time()-t0)))
+
+
+def print_epoch_info(epoch, name, loss, auc, invFpr):
+  logging.info(
+      param.args.name + ' epoch {}. {}: '.format(epoch + 1, name)
+      + 'loss {: >.3E}'.format(loss)
+      + ' -- AUC {: >.3E}'.format(auc)
+      + ' -- 1/FPR {: >.3E}'.format(invFpr)
+  )
+
+def save_model(net):
+  try:
+    with open(path.join(param.args.savedir, param.args.name + '.pkl'), 'wb') as fileout:
+      # net -> cpu in case later running without cuda
+      pickle.dump(net.cpu(), fileout)
+      if (param.args.cuda):
+        net.cuda()
+    logging.warning('Model saved')
+    param.save_args()
+  except:
+    logging.error("Issue saving model or model parameters")
+    exit()
